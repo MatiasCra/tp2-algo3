68d135a2445c789287466e2fc2e76628724b6ce5
MatiasCra <cravchikm@gmail.com>
2023-05-14 19:22:19 -0300

    1er commit

---
+++ ej1/ej1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    cout << "Ej 1" << endl;
+    return 0;
+}
---
+++ ej2/ej2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    cout << "Ej 2" << endl;
+    return 0;
+}
