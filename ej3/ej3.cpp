c71a18c7cbc9cc1d425370b8857a1fefa74dea85
MatiasCra <cravchikm@gmail.com>
2023-05-19 18:11:45 -0300

    ej 3 anda

--- ej3/ej3.cpp
+++ ej3/ej3.cpp
@@ -2,10 +2,13 @@
 #include <vector>
 #include <list>
 #include <algorithm>
+#include <tuple>
 #include <iomanip>
 #include <cmath>
+
 using namespace std;
-enum Tipo_Arista {
+
+enum TipoArista {
     UTP, FO
 };
 
@@ -15,28 +18,27 @@ int cant_modems;
 int precio_UTP;
 int precio_FO;
 int comp_conexas;
-vector<tuple<int,int>> nodos;
-vector<tuple<double,int,int,Tipo_Arista>> aristas; //Costo, arista_1, arista_2, UTP o FO o MODEM
+vector<tuple<int, int>> nodos;
+vector<tuple<double, int, int, TipoArista>> aristas;  // Costo, arista_1, arista_2, UTP o FO o MODEM
 
-struct DSU{
+struct DSU {
 
-    DSU(int n){
+    DSU(int n) {
         padre = rank = vector<int>(n);
-        for(int v = 0; v < n; v++) padre[v] = v;
+        for (int v = 0; v < n; v++) padre[v] = v;
     }
 
-    int find(int v){
-        if(v == padre[v]) return v;
+    int find(int v) {
+        if (v == padre[v]) return v;
         return padre[v] = find(padre[v]);
     }
 
-    void unite(int u, int v){
+    void unite(int u, int v) {
         u = find(u), v = find(v);
-        if(u == v) return;
-        comp_conexas--;
-        if(rank[u] < rank[v]) swap(u,v);
+        if (u == v) return;
+        if (rank[u] < rank[v]) swap(u, v);
         padre[v] = padre[u];
-        rank[u] = max(rank[u],rank[v]+1);
+        rank[u] = max(rank[u], rank[v] + 1);
     }
 
     vector<int> padre;
@@ -47,42 +49,51 @@ struct DSU{
 void leer_input() {
     // Leo test de stdin, cargo las dimensiones, la lista de adyacencia e inicializo los arrays.
     cin >> cant_oficinas >> dist_tope_UTP >> cant_modems >> precio_UTP >> precio_FO;
-    nodos = vector<tuple<int,int>>(cant_oficinas);
+    nodos = vector<tuple<int, int>>(cant_oficinas);
     comp_conexas = cant_oficinas;
+
     for (int i = 0; i < cant_oficinas; ++i) {
         int coord_x, coord_y;
         cin >> coord_x >> coord_y;
-        nodos[i] = make_tuple(coord_x, coord_y);
+        nodos[i] = tuple<int, int>(coord_x, coord_y);
     }
 }
 
-void obtener_aristas(){
-    //Agrego aristas entre todos los nodos
+void obtener_aristas() {
+    // Agrego aristas entre todos los nodos
     for (int i = 0; i < cant_oficinas; ++i) {
-        for (int j = i+1; j < cant_oficinas; ++j) {
-            double dist = sqrt( pow(get<0>(nodos[i]) - get<0>(nodos[j]) ,2) + pow(get<1>(nodos[i]) - get<1>(nodos[j]) ,2));
-            if (dist < dist_tope_UTP){
-                aristas.emplace_back(dist * precio_UTP,i,j,UTP);
+        for (int j = i + 1; j < cant_oficinas; ++j) {
+            double dist = sqrt(
+                    pow(get<0>(nodos[i]) - get<0>(nodos[j]), 2) + pow(get<1>(nodos[i]) - get<1>(nodos[j]), 2));
+
+            if (dist <= dist_tope_UTP) {
+                aristas.emplace_back(dist * precio_UTP, i, j, UTP);
             } else {
-                aristas.emplace_back(dist * precio_FO,i,j,FO);
+                aristas.emplace_back(dist * precio_FO, i, j, FO);
             }
         }
     }
 }
 
 
-int main () {
+int main() {
     int casos_de_test;
     cin >> casos_de_test;
 
     for (int i = 1; i <= casos_de_test; ++i) {
         leer_input();
+        aristas.clear();
         obtener_aristas();
         sort(aristas.begin(), aristas.end());
         DSU dsu(cant_oficinas);
         double costo_UTP = 0;
         double costo_FO = 0;
-        for (auto [c, u, v, tipo]: aristas) {
+        for (tuple<double, int, int, TipoArista> arista: aristas) {
+            double c = get<0>(arista);
+            int u = get<1>(arista);
+            int v = get<2>(arista);
+            TipoArista tipo = get<3>(arista);
+
             if (dsu.find(u) != dsu.find(v)) {
                 dsu.unite(u, v);
                 if (tipo == FO) {
@@ -90,6 +101,7 @@ int main () {
                 } else {
                     costo_UTP += c;
                 }
+
                 comp_conexas--;
                 if (comp_conexas == cant_modems) {
                     break;
@@ -97,7 +109,8 @@ int main () {
             }
         }
         cout << fixed;
-        cout << "Caso #" << i << ": " << costo_UTP << " " << costo_FO << endl;
+        cout << setprecision(3) << "Caso #" << i << ": " << costo_UTP << " " << costo_FO << endl;
     }
+
     return 0;
 }
