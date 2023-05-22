e642dce70d94cf8ed12b5c3ce8de19efad503518
MatiasCra <cravchikm@gmail.com>
2023-05-22 14:59:42 -0300

    kruskal n^2

--- ej3/ej3_n2.cpp
+++ ej3/ej3_n2.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-#define INF numeric_limits<double>::max()
+#define MAXDOUBLE numeric_limits<double>::max()
 
 enum TipoArista {
     UTP, FO
@@ -22,7 +22,7 @@ int precio_UTP;
 int precio_FO;
 int comp_conexas;
 vector<tuple<int, int>> nodos;
-vector<vector<double>> adyacencia;
+vector<vector<pair<double, TipoArista>>> adyacencia;
 
 void leer_input() {
     // Leo test de stdin, cargo las dimensiones, la lista de adyacencia e inicializo los arrays.
@@ -39,7 +39,11 @@ void leer_input() {
 
 void obtener_aristas() {
     // Agrego aristas entre todos los nodos
-    adyacencia = vector<vector<double>>(cant_oficinas, vector<double>(cant_oficinas, 0));
+    adyacencia = vector<vector<pair<double, TipoArista>>>(
+            cant_oficinas, vector<pair<double, TipoArista>>(
+                    cant_oficinas, make_pair(MAXDOUBLE, FO)
+            )
+    );
 
     for (int i = 0; i < cant_oficinas; ++i) {
         for (int j = i + 1; j < cant_oficinas; ++j) {
@@ -47,11 +51,56 @@ void obtener_aristas() {
                     pow(get<0>(nodos[i]) - get<0>(nodos[j]), 2) + pow(get<1>(nodos[i]) - get<1>(nodos[j]), 2));
 
             if (dist <= dist_tope_UTP) {
-                adyacencia[i][j] = dist * precio_UTP;
+                adyacencia[j][i] = adyacencia[i][j] = make_pair(dist * precio_UTP, UTP);
             } else {
-                adyacencia[i][j] = dist * precio_FO;
+                adyacencia[j][i] = adyacencia[i][j] = make_pair(dist * precio_FO, FO);
             }
         }
+    }
+}
+
+pair<int, int> arista_minima(vector<double> &min_coste, vector<bool> &es_representante) {
+    /*
+     * Busco la arista mínima de un arbol del bosque a otro.
+     * */
+
+    // Eligo min entre la min de cada árbol
+    int idx = 0;
+    while (not es_representante[idx]) ++idx;
+    for (int i = idx + 1; i < cant_oficinas; ++i) {
+        if (not es_representante[i]) continue;
+        if (min_coste[i] < min_coste[idx]) idx = i;
+    }
+
+    // La min es una arista del árbol con representante idx, de coste min_coste[idx]. Busco a qué otro árbol conecta
+    double coste = min_coste[idx];
+    int objetivo = 0;
+    for (int i = 0; i < cant_oficinas; ++i) {
+        if (not es_representante[i]) continue;
+        if (adyacencia[idx][i].first == coste) {
+            objetivo = i;
+            break;
+        }
+    }
+
+    return {idx, objetivo};
+}
+
+void
+contraer(pair<int, int> &siguiente_arista, vector<double> &min_coste, vector<bool> &es_representante) {
+    /*
+     * Contraigo a un vértice por árbol del bosque (centro todas las aristas de un árbol en su representate).
+     */
+
+    int u = siguiente_arista.first, v = siguiente_arista.second;
+    es_representante[v] = false;  // árbol de representante v pasa al árbol de representante u
+    min_coste[u] = MAXDOUBLE;
+    // Actualizo aristas del árbol de u para incluir aristas del árbol de v, también la árista mínima que va del árbol a
+    // otro
+    for (int i = 0; i < cant_oficinas; ++i) {
+        if (not es_representante[i] or i == u) continue;
+        adyacencia[i][u] = adyacencia[u][i] = min(adyacencia[u][i], adyacencia[v][i]);
+        min_coste[u] = min(min_coste[u], adyacencia[u][i].first);
     }
 }
 
@@ -66,8 +115,28 @@ int main() {
         double costo_UTP = 0;
         double costo_FO = 0;
 
-        // TODO: Kruskal n**2
+        // Kruskal en O(n^2)
+        vector<bool> es_representante(cant_oficinas, true);  // Un representante por árbol del bosque
+        vector<double> min_coste(cant_oficinas, MAXDOUBLE);  // Min coste de un árbol a otro
+        for (int j = 0; j < cant_oficinas; ++j) {
+            for (int k = j + 1; k < cant_oficinas; ++k) {
+                min_coste[j] = min(min_coste[j], adyacencia[j][k].first);
+                min_coste[k] = min(min_coste[k], adyacencia[k][j].first);
+            }
+        }
 
+        // Elijo aristas. Paro cuando tenga los W árboles en los que irán los modems
+        for (int j = 0; j < cant_oficinas - cant_modems; ++j) {
+            pair<int, int> siguiente_arista = arista_minima(min_coste, es_representante);
+            TipoArista tipo = adyacencia[siguiente_arista.first][siguiente_arista.second].second;
+            double coste = adyacencia[siguiente_arista.first][siguiente_arista.second].first;
+            if (tipo == UTP)
+                costo_UTP += coste;
+            else if (tipo == FO)
+                costo_FO += coste;
+
+            contraer(siguiente_arista, min_coste, es_representante);
+        }
 
         cout << fixed;
         cout << setprecision(3) << "Caso #" << i << ": " << costo_UTP << " " << costo_FO << endl;
